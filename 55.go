1e90fc683036c3efff5e947975941f6297af8941
eric <chang.eric@degames.com.tw>
2024-05-06 18:38:59 +0800

    Update 55

--- 55.go
+++ 55.go
@@ -7,13 +7,16 @@ package main
 // Output: false
 // Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
 
-// 2,3,1,1,4
 func canJump(nums []int) bool {
-	max_len := 0
+	// nums = [0]
+    if nums[0]==0 && len(nums) == 1 {
+        return true
+    }
+    max_len := 0
 	for i, v := range nums {
 		if max_len >= i && i+v > max_len{
 			max_len =i+v	
 		}	
 	}	
-	return max_len >= len(nums)
+	return max_len >= len(nums)-1
 }
